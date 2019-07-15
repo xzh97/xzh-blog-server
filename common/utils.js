c8ac84b2c435c1d5677fdebaed3d7783b2da7a9b
xzh <xzh19971005@163.com>
2019-07-15 23:09:53 +0800

    commit

--- common/utils.js
+++ common/utils.js
@@ -1,15 +1,15 @@
 /**
- * @summary 工具库
+ * @description 工具库
  * @author xzh
  *
  * Created at     : 2019-06-06 23:24:26 
- * Last modified  : 2019-07-09 20:56:04
+ * Last modified  : 2019-07-15 22:45:58
  */
 
 /**
  * @description 日期格式化
- * @param {} date 
- * @param {*} format 
+ * @param {} date
+ * @param {*} format
  */
 const dateFormat = (date, format='yyyy-MM-dd') => {
     let d = new Date(date);
@@ -70,7 +70,7 @@ const pagination = (total,data,currentPage,size) => {
 }
 
 /** 
- * @summary 判断是否是空对象
+ * @description 判断是否是空对象
  * @param {object} 判断对象
  * @return {boolean}
 */
@@ -82,7 +82,7 @@ const isEmptyObj = (obj) => {
 }
 
 /** 
- * @summary 判断是否是空数组
+ * @description 判断是否是空数组
  * @param {object} 判断数组
  * @return {boolean}
 */
@@ -92,7 +92,7 @@ const isEmptyArray = (arr) => {
 }
 
 /** 
- * @summary 去掉blog内容里的标签 
+ * @description 去掉blog内容里的标签 
  * @param {string} str
  * @return {string}
 */
@@ -100,18 +100,59 @@ const removeTag = (str) => {
     console.log(str);
     return str.replace(/<[^>]+>/g, "").substr(0,100);
 }
-const handleError = (fn = () => {}) =>{
-    try {
-        fn && fn()
-    } catch (error) {
-        return error
-    }
+
+/**
+ * @param {String} val 要处理的key
+ * @param {String} char 要替换的字符
+ * @description 驼峰转下划线
+ * @return 处理过的key
+ */
+const str2Underline = (val,char='_') => {
+    return val.replace(/([A-Z])/g,`${char}$1`).toLowerCase();
 }
+
+/**
+ * @param {String} val 要处理的key
+ * @description 下划线转驼峰
+ * @return 处理过的key
+ */
+const underline2Str = (val) => {
+    return val.replace(/\_(\w)/g, (all, letter) => {
+        return letter.toUpperCase()
+      })
+}
+
+/**
+ * @param {String} obj 要处理的对象
+ * @param {String} char 要替换的字符
+ * @param {Boolean} toUnderLine 要替换的字符
+ * @description 原对象 下划线转驼峰或者驼峰转下划线  默认驼峰转下划线
+ * @return 处理过的key
+ */
+const replaceUnderlineOrCamel = (obj = {}, char='_', toUnderLine = true) => {
+    let arr = Object.keys(obj).filter(item => ~item.indexOf(char));
+    let methodMap = {
+        toUnderline:str2Underline,
+        toStr:underline2Str
+    }
+    let method = methodMap[toUnderLine ? 'toUnderline' : 'toStr']
+    arr.map(item => {
+        const val = obj[item];
+        const key = method(item);
+        obj[key] = val;
+        delete obj[item];
+    })
+    return obj;
+}
+
+
 module.exports = {
     dateFormat,
     pagination,
     isEmptyObj,
     removeTag,
     isEmptyArray,
-    handleError,
+    str2Underline,
+    underline2Str,
+    replaceUnderlineOrCamel,
 }
