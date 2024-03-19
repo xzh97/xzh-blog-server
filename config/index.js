306e0d3ba3d57447a54e298cbb729288f66c7c92
xizh <995194437@qq.com>
2024-03-19 16:43:28 +0800

    refactor: nest init

--- config/index.js
+++
@@ -1,5 +0,0 @@
-const devConfig = require('./dev');
-// const prodConfig = require('./prod');
-console.log('current env: ', process.env.NODE_ENV);
-let currentConfig = process.env.NODE_ENV === 'dev' ? devConfig : prodConfig
-module.exports = currentConfig
